af28a7564bffd49b29b5523ace0373bd8f1c0e06
Harris Weeks <root@hweeks.com>
2020-03-01 19:24:29 -0800

    feat: add basic layout

---
+++ static/app/comps/Header/index.js
@@ -0,0 +1,7 @@
+import { html } from 'https://unpkg.com/htm/preact/standalone.module.js';
+
+export const Header = props => html`<header class="wrap-header">
+  <h1 class="big-header">Modern Measurements</h1>
+  <p>measurements to make god cry</p>
+</header>
+`
---
+++ static/app/comps/Link/index.js
@@ -0,0 +1,3 @@
+import { html } from 'https://unpkg.com/htm/preact/standalone.module.js';
+
+export const Link = props => html`<a target="_BLANK" class="big-link" ...${props} />`

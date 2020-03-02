ed4631e84b09aa2f26c858c5a4b5b5f5c89d501e
Ricky Romero <ricky.romero@gmail.com>
2020-03-01 17:03:09 -0800

    feat: Conversion logic

---
+++ static/scripts/convert.js
@@ -0,0 +1,42 @@
+const measures = [
+  {
+    unit: 'football field',
+    property: 'length',
+    factor: 109.7
+  }, {
+    unit: 'swimming pool',
+    property: 'volume',
+    factor: 2500000
+  }, {
+    unit: 'football field',
+    property: 'area',
+    factor: 5351
+  }, {
+    unit: 'hectare',
+    property: 'area',
+    factor: 10000
+  }, {
+    unit: 'paperclip',
+    property: 'mass',
+    factor: 1
+  }, {
+    unit: 'troy ounce',
+    property: 'mass',
+    factor: 31.1034768
+  }
+]
+
+const lookupFactor = ({ property, unit }) => {
+  return measures.find(
+    measure => measure.property === property && measure.unit === unit
+  )
+}
+
+const convert = ({ property, qty, fromUnit, toUnit }) => {
+  const fromFactor = lookupFactor({ property, unit: fromUnit })
+  const toFactor = lookupFactor({ property, unit: toUnit })
+
+  return qty * fromUnit / toUnit
+}
+
+export default convert
